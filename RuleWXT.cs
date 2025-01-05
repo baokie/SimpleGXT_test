5d3c115758f5c0813757062274eab1e1a0585fb3
baokie <2741241624@qq.com>
2025-01-05 23:22:10 +0800

    newtest

---
+++ RuleWXT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 简单关系图_测试_
+{
+    public static class RuleWXT
+    {
+        public static void getRule(string[] alldata)
+        {
+            int paddingW = 20, paddingH = 20;
+
+        }
+    }
+}
